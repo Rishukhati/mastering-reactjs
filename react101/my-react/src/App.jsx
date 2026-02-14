c284dac005164ee27f20e8878ab989156df8182e
Rishabh singh <rishabhsingh.block@gmail.com>
2026-02-14 14:59:03 +0530

    till usergreeting

---
+++ react101/my-react/src/App.jsx
@@ -0,0 +1,13 @@
+
+import UserGreeting from './userGreeting.jsx'
+
+function App() {
+  return (
+    <>
+
+      <UserGreeting isLoggedIn={true} username='Rishabh' />
+    </>
+  )
+}
+
+export default App
---
+++ react101/my-react/src/rishu.jsx
@@ -0,0 +1,9 @@
+
+function Rishu() {
+    return(
+        <header>
+            <h1>Myy website</h1>
+        </header>
+    )
+}
+export default Rishu
